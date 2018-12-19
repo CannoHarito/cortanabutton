a13cfa1457a50ad6ee085c9e67c5adc0810aa11a
CannoHarito <45878603+CannoHarito@users.noreply.github.com>
2018-12-19 17:53:19 +0900

    keycode選択と設定保存

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const { ipcRenderer } = require('electron')
-
-document.querySelector('#btnExit').addEventListener('click', () => {
-    ipcRenderer.send('terminate')
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#btnExit').addEventListener('click', () => {
+        ipcRenderer.send('terminate')
+    })
+    document.querySelector('#slcKeycode').addEventListener('change', (e) => {
+        document.querySelector('#keycodeState').innerHTML = "Changing..."
+        ipcRenderer.send('keycode', event.target.value)
+    })
+    ipcRenderer.send('state')
+})
+ipcRenderer.on('stateReply', (e, state) => {
+    if (state.keycode) {
+        document.querySelector('#slcKeycode').value = state.keycode
+        document.querySelector('#keycodeState').innerHTML
+            = state.registered ? 'Registered' : 'Failed. Please turn off your media player and '
+                + '<button onclick="document.querySelector(\'#slcKeycode\').dispatchEvent(new Event(\'change\'))">Retry</button>'
+    } else {
+        document.querySelector('#slcKeycode').value = ""
+        document.querySelector('#keycodeState').innerHTML = ""
+    }
 })
--- main.js
+++ main.js
@@ -3,37 +3,13 @@ const ICONFAILE = __dirname + '/images/iconFaile.png'
 const { ipcMain, globalShortcut } = require('electron')
 const exec = require('child_process').exec
 const menubar = require('menubar')
-
+const Store = require('electron-store')
+const store = new Store()
 const mb = menubar({ icon: ICONFAILE })
+const state = { registered: false }
 
 mb.on('ready', () => {
   console.log('app is ready')
-  // Register a 'CommandOrControl+X' shortcut listener.
-  globalShortcut.unregister('MediaPreviousTrack')
-  const ret = globalShortcut.register('MediaPreviousTrack', () => {
-    console.log('MediaPreviousTrack is pressed')
-    exec('start ms-cortana://Reactive/?ListeningMode=True', (error, stdout, stderr) => {
-      if (stdout) {
-        console.log('stdout: ' + stdout)
-      }
-      if (stderr) {
-        console.log('stderr: ' + stderr)
-      }
-      if (error !== null) {
-        console.log('Exec error: ' + error)
-      }
-    })
-  })
-
-  if (ret) {
-    mb.tray.setImage(ICON)
-  } else {
-    console.log('registration failed')
-  }
-
-  // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('MediaPreviousTrack'))
-
   mb.app.on('will-quit', () => {
     // Unregister all shortcuts.
     globalShortcut.unregisterAll()
@@ -43,12 +19,50 @@ mb.on('ready', () => {
     // Stop quitting app.
   })
 
+  state.keycode = store.get('keycode', false)
+  if (state.keycode) {
+    setGlobalShortcut(state.keycode)
+  }
+  if (!state.registered) mb.showWindow()
 })
 
 mb.on('after-hide', () => {
+  // release mb.window resource
   mb.window.close()
 })
 
-ipcMain.on('terminate', function terminate() {
+ipcMain.on('terminate', () => {
   mb.app.quit()
 })
+ipcMain.on('state', (e) => {
+  e.sender.send('stateReply', state)
+})
+ipcMain.on('keycode', (e, newKeycode) => {
+  console.log('new keycode is ' + newKeycode)
+  state.keycode = newKeycode
+  store.set('keycode', newKeycode)
+  delGlobalShortcut()
+  if (newKeycode) setGlobalShortcut(newKeycode)
+  e.sender.send('stateReply', state)
+})
+const delGlobalShortcut = (keycode) => {
+  state.registered = false
+  mb.tray.setImage(ICONFAILE)
+  if (keycode) globalShortcut.unregister(keycode)
+  else globalShortcut.unregisterAll()
+}
+const setGlobalShortcut = (keycode) => {
+  // Register a 'keycode' shortcut listener.
+  const ret = globalShortcut.register(keycode, (e) => {
+    exec('start ms-cortana://Reactive/?ListeningMode=True', (error, stdout, stderr) => {
+      if (error) console.error(error)
+      if (stdout) console.log(stdout)
+    })
+  })
+  if (ret) {
+    mb.tray.setImage(ICON)
+  } else {
+    console.log('registration failed')
+  }
+  state.registered = ret
+}
